84aeeca49014ae234de5a5a05157c2e69fc3c4fb
Ubaid-A <wafaubaid3@gmail.com>
2024-09-10 08:50:19 +0500

    quiz styling done html and css

---
+++ script.js
@@ -0,0 +1,47 @@
+const question =[
+    {
+        question: "Html stand for ?",
+        answers: [
+            {text: "HyperText Module Language", correct: flase},
+            {text: "HyperText Markup Language", correct: true},
+            {text: "HyperTool Markup Language", correct: flase},
+            {text: "Hyper Transfer Markup Language", correct: flase},
+        ]
+    },
+    {
+        question: "CSS stands for?",
+        answers: [
+            {"text": "Cascading Style Sheets", "correct": true},
+            {"text": "Computer Style Sheets", "correct": false},
+            {"text": "Creative Style Sheets", "correct": false},
+            {"text": "Colorful Style Sheets", "correct": false}
+        ]
+    },
+    {
+        question: "Which property is used to change the text color of an element?",
+        answers: [
+            {"text": "color", "correct": true},
+            {"text": "text-color", "correct": false},
+            {"text": "font-color", "correct": false},
+            {"text": "background-color", "correct": false}
+        ]
+    },
+    {
+        question: "How do you add a background color in CSS?",
+        answers: [
+            {"text": "background-color", "correct": true},
+            {"text": "color", "correct": false},
+            {"text": "bg-color", "correct": false},
+            {"text": "background", "correct": false}
+        ]
+    },
+    {
+        question: "Which CSS property controls the text size?",
+        answers: [
+            {"text": "font-size", "correct": true},
+            {"text": "text-style", "correct": false},
+            {"text": "font-weight", "correct": false},
+            {"text": "text-size", "correct": false}
+        ]
+    }
+];
